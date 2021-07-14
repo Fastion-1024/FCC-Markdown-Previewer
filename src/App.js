18a6e95e54c4c77ab87eb83c8b0b008bcd0b8e5d
Fastion-1024 <dsprice1989@outlook.com>
2021-07-14 15:17:31 +0100

    Updated App Component - Added Dock Container / Editor and Previewer Components. - Added Header.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import DockContainer from './Components/DockContainer';
+import Editor from './Components/Editor';
+import Previewer from './Components/Previewer';
+
 function App() {
-    return <main></main>;
+    return (
+        <main>
+            <header className='header'>
+                <h1>
+                    <span>{'{ '}</span>MarkdownPreviewer<span>{' }'}</span>
+                </h1>
+            </header>
+            <DockContainer className='container'>
+                <Editor />
+                <Previewer />
+            </DockContainer>
+        </main>
+    );
 }
 
 export default App;
