28e82513424e38e9bf2cc0412fcc77e16391dfd7
Fastion-1024 <dsprice1989@outlook.com>
2021-07-16 13:31:04 +0100

    Fixed Dock Bug. - Fixed a bug with padding on editor and previewer. - Fixed comments for correct layout.

--- src/Components/DockContainer.js
+++ src/Components/DockContainer.js
@@ -25,39 +25,39 @@ const DockContainer = ({ children }) => {
     }
 
     if (isRow) {
-        // Dock Bottom
+        // Dock Right
         if (isReversed) {
             return (
                 <div className='dock-container row-reverse'>
-                    <div className='panel1'>{children[0]}</div>
-                    <div className='panel2'>{children[1]}</div>
+                    <div className='panel1 row-reverse'>{children[0]}</div>
+                    <div className='panel2 row-reverse'>{children[1]}</div>
                 </div>
             );
         }
-        // Dock Top
+        // Dock Left
         return (
             <div className='dock-container row'>
-                <div className='panel1'>{children[0]}</div>
-                <div className='panel2'>{children[1]}</div>
+                <div className='panel1 row'>{children[0]}</div>
+                <div className='panel2 row'>{children[1]}</div>
             </div>
         );
     }
 
     if (isReversed) {
-        // Dock Right
+        // Dock Bottom
         return (
             <div className='dock-container column-reverse'>
-                <div className='panel1'>{children[0]}</div>
-                <div className='panel2'>{children[1]}</div>
+                <div className='panel1 column-reverse'>{children[0]}</div>
+                <div className='panel2 column-reverse'>{children[1]}</div>
             </div>
         );
     }
 
-    // Dock Left
+    // Dock Top
     return (
         <div className='dock-container column'>
-            <div className='panel1'>{children[0]}</div>
-            <div className='panel2'>{children[1]}</div>
+            <div className='panel1 column'>{children[0]}</div>
+            <div className='panel2 column'>{children[1]}</div>
         </div>
     );
 };
